5bb1edc449ccc87ea4074c503d71d7d9553338ce
onyebekehcode <onyebekehsamuel@gmail.com>
2025-11-04 15:37:38 +0000

    word count

---
+++ app.js
@@ -0,0 +1,23 @@
+const input = document.getElementById("text-input");
+const button = document.getElementById("check-btn");
+const result = document.getElementById("result");
+let output = ""
+let cleanedString = ""
+
+
+const checkValidInput = ()=>{
+  if (input.value === ""){
+    alert("yo! what do you want me to count?")
+  }else{
+    countWords()
+  }
+}
+button.addEventListener("click", checkValidInput)
+
+function countWords(){
+output = [input.value].toString()
+console.log(output)
+let count = output.split(" ")
+ count = `Your article contains ${output.length} words`
+result.textContent = count
+}
